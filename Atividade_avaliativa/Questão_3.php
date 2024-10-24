e43eb4f9d38675f58906492d5d71919b40b5f23c
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2024-10-24 15:09:45 -0300

    Create Questão_3.php

---
+++ Atividade_avaliativa/Questão_3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Questão-3</title>
+</head>
+<body>
+<h1 id="meu_titulo">Questão_3</h1>
+
+<script>
+
+titulo=document.getElementById("meu_titulo");
+titulo.innerText = "Mengão";
+titulo.style.color = "red";
+titulo.style.fontSize = "48px";
+
+</script>
+
+</body>
+</html>
