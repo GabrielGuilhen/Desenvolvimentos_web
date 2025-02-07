569fb7e6b44d7e4dd80ef97ef581fe7413b27648
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-02-07 14:58:33 -0300

    Create app.js

---
+++ area_administrativa/app.js
@@ -0,0 +1,19 @@
+// Função para incluir arquivos HTML dinamicamente
+function includeHTML(elementId, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => document.getElementById(elementId).innerHTML = data)
+        .catch(error => console.log("Erro ao carregar " + file + ": " + error));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Incluir o menu lateral e a navbar
+    includeHTML("container-sidebar", "sidebar.html");
+    includeHTML("container-navbar", "navbar.html");
+    
+});
+
+function toggleMenu(element){
+    // Alternar a visibilidade do menu em telas pequenas
+    d
